60796185c47d7478a61b5366270c8f2ae7cf7da1
Negru95 <daninegru8@gmail.com>
2023-06-09 16:24:11 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    hangmanGame.initialize();
+  });
+  
+  const newGameBtn = document.getElementById("new-game-btn");
+  newGameBtn.addEventListener("click", () => {
+    hangmanGame.resetGame();
+  });
+  newGameBtn.classList.add("special");
+  
