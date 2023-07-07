ccdc163eebd758e1cea173ebef4ce9452afcb75e
Negru95 <daninegru8@gmail.com>
2023-07-07 08:46:51 +0300

    12

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", () => {
   hangmanGame.initialize();
-});
 
 const newGameBtn = document.getElementById("new-game-btn");
 newGameBtn.addEventListener("click", () => {
